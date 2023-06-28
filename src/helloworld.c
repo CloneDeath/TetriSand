cf507c0ed39ba77a401e31d6a2ecf499fb727c07
Nicholas Rodine <halfofastaple@gmail.com>
2023-06-28 14:54:43 -0500

    Initial commit

---
+++ src/helloworld.c
@@ -0,0 +1,17 @@
+#include <gb/gb.h>
+#include <gb/drawing.h>
+#include <stdio.h>
+
+int main(){
+    // start screen (press start to continue)
+    gotogxy(3,8); // set text start position
+    gprintf("Press Start...");
+    waitpad(J_START); // wait for start
+    gotogxy(3,8); // reset text start position
+    gprintf("              "); // clear message
+
+    // display message to screen
+    gotogxy(3,8); // set text start position
+    gprintf("Hello World!"); // print message
+    return 0;
+}
