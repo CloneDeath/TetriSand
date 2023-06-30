f9c16f1b6fbf3d372088a7326dfbab16330d7cfb
Nicholas Rodine <halfofastaple@gmail.com>
2023-06-30 05:07:00 -0500

    changed name of tiles to border_tiles

--- src/tile_zone.c
+++ src/tile_zone.c
@@ -10,28 +10,28 @@ struct tile_zone* new_tile_zone(uint8_t x, uint8_t y, uint8_t width, uint8_t hei
     tz->y = y;
     tz->width = width;
     tz->height = height;
-    tz->tiles = alloc_tile_set(9);
+    tz->border_tiles = alloc_tile_set(9);
 
-    set_bkg_data(tz->tiles->start, tz->tiles->count, TileBorder);
+    set_bkg_data(tz->border_tiles->start, tz->border_tiles->count, TileBorder);
 
-    set_bkg_tile_xy(x, y, tz->tiles->start + 0); // Top-Left
-    set_bkg_tile_xy(x + width - 1, y, tz->tiles->start + 2); // Top-Right
-    set_bkg_tile_xy(x, y + height - 1, tz->tiles->start + 5); // Bottom-Left
-    set_bkg_tile_xy(x + width - 1, y + height - 1, tz->tiles->start + 7); // Bottom-Right
+    set_bkg_tile_xy(x, y, tz->border_tiles->start + 0); // Top-Left
+    set_bkg_tile_xy(x + width - 1, y, tz->border_tiles->start + 2); // Top-Right
+    set_bkg_tile_xy(x, y + height - 1, tz->border_tiles->start + 5); // Bottom-Left
+    set_bkg_tile_xy(x + width - 1, y + height - 1, tz->border_tiles->start + 7); // Bottom-Right
 
     for (uint8_t i = 1; i < width-1; i++){
-        set_bkg_tile_xy(x + i, y, tz->tiles->start + 1); // Top
-        set_bkg_tile_xy(x + i, y + height - 1, tz->tiles->start + 6); // Bottom
+        set_bkg_tile_xy(x + i, y, tz->border_tiles->start + 1); // Top
+        set_bkg_tile_xy(x + i, y + height - 1, tz->border_tiles->start + 6); // Bottom
     }
     for (uint8_t i = 1; i < height-1; i++){
-        set_bkg_tile_xy(x, y + i, tz->tiles->start + 3); // Left
-        set_bkg_tile_xy(x + width - 1, y + i, tz->tiles->start + 4); // Right
+        set_bkg_tile_xy(x, y + i, tz->border_tiles->start + 3); // Left
+        set_bkg_tile_xy(x + width - 1, y + i, tz->border_tiles->start + 4); // Right
     }
 
     return tz;
 }
 
 void delete_tile_zone(struct tile_zone* this) {
-    free_tile_set(this->tiles);
+    free_tile_set(this->border_tiles);
     free(this);
 }
--- src/tile_zone.h
+++ src/tile_zone.h
@@ -7,7 +7,7 @@ struct tile_zone {
     uint8_t y;
     uint8_t width;
     uint8_t height;
-    struct tile_set* tiles;
+    struct tile_set* border_tiles;
 };
 
 struct tile_zone* new_tile_zone(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
