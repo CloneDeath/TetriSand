e8430b09f21a8c5fd36b79f65f80fccd283a6890
Nicholas Rodine <halfofastaple@gmail.com>
2023-06-30 05:05:01 -0500

    added tiles to the tile zone object, and freed it too

--- src/tile_zone.c
+++ src/tile_zone.c
@@ -10,28 +10,28 @@ struct tile_zone* new_tile_zone(uint8_t x, uint8_t y, uint8_t width, uint8_t hei
     tz->y = y;
     tz->width = width;
     tz->height = height;
+    tz->tiles = alloc_tile_set(9);
 
-    struct tile_set* tiles = alloc_tile_set(9);
+    set_bkg_data(tz->tiles->start, tz->tiles->count, TileBorder);
 
-    set_bkg_data(tiles->start, tiles->count, TileBorder);
-
-    set_bkg_tile_xy(x, y, tiles->start + 0); // Top-Left
-    set_bkg_tile_xy(x + width - 1, y, tiles->start + 2); // Top-Right
-    set_bkg_tile_xy(x, y + height - 1, tiles->start + 5); // Bottom-Left
-    set_bkg_tile_xy(x + width - 1, y + height - 1, tiles->start + 7); // Bottom-Right
+    set_bkg_tile_xy(x, y, tz->tiles->start + 0); // Top-Left
+    set_bkg_tile_xy(x + width - 1, y, tz->tiles->start + 2); // Top-Right
+    set_bkg_tile_xy(x, y + height - 1, tz->tiles->start + 5); // Bottom-Left
+    set_bkg_tile_xy(x + width - 1, y + height - 1, tz->tiles->start + 7); // Bottom-Right
 
     for (uint8_t i = 1; i < width-1; i++){
-        set_bkg_tile_xy(x + i, y, tiles->start + 1); // Top
-        set_bkg_tile_xy(x + i, y + height - 1, tiles->start + 6); // Bottom
+        set_bkg_tile_xy(x + i, y, tz->tiles->start + 1); // Top
+        set_bkg_tile_xy(x + i, y + height - 1, tz->tiles->start + 6); // Bottom
     }
     for (uint8_t i = 1; i < height-1; i++){
-        set_bkg_tile_xy(x, y + i, tiles->start + 3); // Left
-        set_bkg_tile_xy(x + width - 1, y + i, tiles->start + 4); // Right
+        set_bkg_tile_xy(x, y + i, tz->tiles->start + 3); // Left
+        set_bkg_tile_xy(x + width - 1, y + i, tz->tiles->start + 4); // Right
     }
 
     return tz;
 }
 
 void delete_tile_zone(struct tile_zone* this) {
+    free_tile_set(this->tiles);
     free(this);
 }
