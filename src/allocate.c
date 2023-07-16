c026db7993aec71d85ae66493fae1fa73ebaafb7
Nicholas Rodine <halfofastaple@gmail.com>
2023-07-16 07:11:43 -0500

    adjusted allocate to give more information, and to hide sprites.

--- src/allocate.c
+++ src/allocate.c
@@ -1,11 +1,14 @@
 #include "allocate.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <gb/gb.h>
 
 void* allocate(size_t size) {
     void* data = malloc(size);
     if (data == NULL) {
-        printf("OUT OF MEMORY - allocate");
+        HIDE_SPRITES;
+        printf("OUT OF MEMORY\n");
+        printf("allocate 0x%x", size);
         exit(-1);
     }
     return data;
@@ -14,7 +17,9 @@ void* allocate(size_t size) {
 void* allocate_array(size_t count, size_t size) {
     void* data = calloc(count, size);
     if (data == NULL) {
-        printf("OUT OF MEMORY - allocate_array");
+        HIDE_SPRITES;
+        printf("OUT OF MEMORY\n");
+        printf("allocate_array %uh x 0x%x", count, size);
         exit(-1);
     }
     return data;
