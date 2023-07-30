972f57137b81d393a5db94867f8c3796ef6cc560
Nicholas Rodine <halfofastaple@gmail.com>
2023-07-30 11:05:04 -0500

    cleared out sand that makes a line

--- src/gameplay/sand_zone.c
+++ src/gameplay/sand_zone.c
@@ -140,6 +140,7 @@ static inline bool _check_for_start_to_end_path_for_chain(sand_zone* this, sand_
     if (sand_chain_list__contains_x(processed, this->width * 8 - 1)) {
         sand_chain_reference* current = processed->_items;
         while (current != NULL) {
+            _set_sand_color_column(this, current->x, current->chain->y, current->chain->length, DMG_WHITE);
             current->chain->value = 0;
             current->chain->length = 0;
             current = current->next;
